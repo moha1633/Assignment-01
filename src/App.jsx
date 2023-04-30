79f2fa9d7b419481da8256e5f8807199897c4bb7
mohamed abdi <moha1633>
2023-04-30 19:57:08 +0200

    this is last comment of the Assignment and today add some the button that can switch the 3 articles

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,89 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import laker from './assets/laker.jpg'
+
+function getActiveClassName (activeArticle,currentArticle) {
+  if (activeArticle === currentArticle){
+    return 'show-article'
+  }else {
+    return 'hide-article'
+  }
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+ const [activeArticle, setActiveArticle] =useState('First Article')
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+   <>
+
+   <div className={`active ${getActiveClassName(activeArticle,'First Article')}`}>
+
+    <h1>About</h1>
+    <p>is  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+    is  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+    is  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+    is  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+    </p>
+    <img className= 'laker' src={laker} alt="" />
+
+    
+   </div>
+   
+   
+   <div className={`active ${getActiveClassName(activeArticle,'Second Article')}`}>
+
+    <h1>Idea</h1>
+    <p> It was Tourism webbsite that i build html and css and it was my first project they call safari
+    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard 
+    dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen 
+    book. 
+    It has survived not only five centuries,  ,</p>
+   </div>
+
+
+
+
+   <div className={`active ${getActiveClassName(activeArticle,'Third Article')}`}>
+
+    <h1>Design</h1>
+    <p> It was Tourism webbsite that i build html and css and it was my first project they call safari
+    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard 
+    dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen 
+    book. 
+  
+
+    It has survived not only five centuries,  ,</p>
+   
+
+  
+   </div>
+
+
+
+   <div className='article'>
+        <button onClick={() => {
+          if (activeArticle === 'First Article'){
+            setActiveArticle('Second Article') ;
+          } else  if(activeArticle === 'Second Article') {
+            setActiveArticle('Third Article')
+          } else{
+            setActiveArticle('First Article') ;
+           
+          }
+        }}>switch Article </button>
+        
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+   
+   
+   
+   </>
+
+   
+    
+
+
+    
   )
 }
 
